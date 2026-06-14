2c7497b28836c1f48f2c4099d48f09fc8d5328bd
IvanSanchez18 <ivan.sanchez.moreno@alumnojoyfe.iepgroup.es>
2026-06-15 00:27:59 +0200

    deploy

---
+++ assets/GameEndGuard-oWQkGnGI.js
@@ -0,0 +1,1 @@
+import{a as r,r as t}from"./index-C7FbSk7u.js";const u=({session:a})=>{const e=r();return t.useEffect(()=>{a&&a.week>a.max_weeks&&e(`/mygm/finale/${a.id}`,{replace:!0})},[a,e]),null};export{u as G};
---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/tfg-2daw-ivan/sw.js', { scope: '/tfg-2daw-ivan/' })})}
