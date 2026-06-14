8b77c31173c198ffcf0d145a82043855f8b2c37d
IvanSanchez18 <ivan.sanchez.moreno@alumnojoyfe.iepgroup.es>
2026-06-15 01:57:49 +0200

    deploy

--- assets/GameEndGuard-DRm2KaVp.js
+++
@@ -1,1 +0,0 @@
-import{a as r,r as t}from"./index-CEyK01MU.js";const u=({session:a})=>{const e=r();return t.useEffect(()=>{a&&a.week>a.max_weeks&&e(`/mygm/finale/${a.id}`,{replace:!0})},[a,e]),null};export{u as G};
